788ee0c1a3e478810be5611ac7bb22edd5b8115e
DAM108 <DAM108@C118.iesataulfo.local>
2023-03-20 17:24:03 +0100

    Segundo commit, se añadio un comentario y se cambio el mensaje de salida

--- Proyecto_1/src/paquete1/Principal.java
+++ Proyecto_1/src/paquete1/Principal.java
@@ -4,6 +4,7 @@ public class Principal
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hola mundo!");
+		//TODO: Hay que hacer mas cambios
+		System.out.println("Otro mensaje diferente");
 	}
 }
